16a87a3a5acab68a929b26e1f39ba1963f0333c3
urlapuga <urlapuga@gmail.com>
2017-05-26 11:45:02 +0300

    easier=better

--- src/Main.kt
+++ src/Main.kt
@@ -2,39 +2,34 @@ enum class CITY { KIEV, ODESSA, KHARKOV }
 
 var hotelList: ArrayList<Hotel> = ArrayList()
 var userMap: HashMap<String, User> = HashMap()
-var roomList: ArrayList<User?> = arrayListOf(null, null, null, null, null, null, null, null, null)
-var hotel: Hotel? = null
 
 data class User(var name: String, var pass: String) {
-    fun add(login: String) = if (userMap.containsKey(login)) null else userMap.put(login, this)
-    fun del(login: String) = if (userMap.containsKey(login)) Finder().findUserByLogin(login)?.let { userMap.remove(login, it) } else false
-    fun edit(login: String) = if (userMap.containsKey(login)) userMap.put(login, this) else null
+    fun User(login: String) = userMap[login]
+    fun add(login: String) = userMap.putIfAbsent(login, this)
+    fun del(login: String) = userMap.remove(login)
+    fun edit(login: String) = userMap.computeIfPresent(login, { _, _ -> userMap.put(login, this)!! })
 }
 
-data class Hotel(var name: String, val city: CITY, var rooms: ArrayList<User?>) {
+data class Hotel(var name: String, var city: CITY, var rooms: ArrayList<User?> = arrayListOf(null, null, null, null, null, null, null, null, null)) {
+    fun Hotel(id: Int) = hotelList.elementAtOrNull(id)
+    fun Hotel(name: String) = hotelList.filter { h -> h.name == name }
+    fun Hotel(city: CITY) = hotelList.filter { h -> h.city == city }
     fun add() = hotelList.add(this)
-    fun del() = if (hotelList.contains(this)) hotelList.remove(this) else false
+    fun del(id: Int) = hotelList[id].let { hotelList.removeAt(id) }
     fun edit(id: Int) = hotelList.add(id, this)
-}
-
-class Finder() {
-    fun findHotelByName(name: String) = hotelList.filter { h -> h.name == name }
-    fun findHotelByCity(city: CITY) = hotelList.filter { h -> h.city == city }
-    fun findHotelById(id: Int) = hotelList.elementAtOrNull(id)
-    fun findUserByLogin(login: String) = userMap.get(login)
-}
-
-class Control(var h: Hotel) {
-    fun roomAdd(number: Int) = h.rooms[number].let { h.rooms[number] = null }
-    fun deleteRoom(number: Int) = h.rooms[number].let { h.rooms.removeAt(number) }
-    fun getFreeRooms() = h.rooms.filter { r -> r == null }
-    fun unbookRoom(number: Int) = h.rooms[number]?.let { h.rooms[number] = null }
-    fun bookRoom(number: Int, user: User) = if (h.rooms[number] == null) h.rooms[number] = user else null
+    fun roomAdd(number: Int) = this.rooms[number].let { this.rooms[number] = null }
+    fun deleteRoom(number: Int) = this.rooms[number].let { this.rooms.removeAt(number) }
+    fun getFreeRooms() = this.rooms.filter { it == null }
+    fun unbookRoom(number: Int) = this.rooms[number].let { this.rooms[number] = null }
+    fun bookRoom(number: Int, user: User) = if (this.rooms[number] == null) this.rooms[number] = user else null
 }
 
 fun main(args: Array<String>) {
+
     for (i in 1..10) {
-        userMap.put("login$i", User("name$i", "pass$i"))
-        hotelList.add(Hotel("name$i", CITY.KHARKOV, roomList))
+        User("name$i", "pass$i").add("login$i")
+        Hotel("name$i", CITY.KHARKOV).add()
     }
+    println(hotelList)
 }
+
